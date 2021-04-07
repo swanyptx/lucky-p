977d7fc642343acd87f547d2a685d2780692034b
Swany <swany59200@icloud.com>
2021-04-07 15:50:49 +0100

    Update Calendar component

---
+++ components/Calendar.js
@@ -0,0 +1,17 @@
+export default function Calendar() {
+    return(
+        <div className="events flex h-screen justify-center items-center">
+            <div className="container">
+                        <h2 className="text-center p-10">Nos événements</h2>
+                <div className="md:flex justify-center items-center">
+                    <div className="calendar m-10 h-48 bg-gray-400">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, fugit.</p>
+                    </div>
+                    <div className="text-events">
+                        <p className="text-center">Lorem ipsum dolor sit amet consectetur </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,7 @@
-import Head from 'next/head'
-
+import Calendar from '../components/Calendar'
 
 export default function Home() {
   return (
-    <div className="">
-      <Head>
-        <title>La Luck</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
-      </Head>
-      
-      <div className="container">
-          <h1>Bonsoir</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate vitae quidem animi ea. Repellendus accusantium ut sed minus ipsam molestiae. Corporis nobis id suscipit consequuntur odit commodi ex adipisci labore!</p>
-      </div>
-    </div>
+    <Calendar />
   )
 }
