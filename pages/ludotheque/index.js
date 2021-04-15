45769941277f8fcef548b087ecc58afabe1828a6
Timothé <timothegauquelin@gmail.com>
2021-04-15 16:52:49 +0200

    Update Ludotheque Search Page

--- pages/ludotheque/index.js
+++ pages/ludotheque/index.js
@@ -65,7 +65,6 @@ const Ludotheque = ({ allGamesFromAPI }) => {
             </Head>
 
             <Header></Header>
-
             <section className="ludotheque">
                 <div className="container py-20 sm:py-10 flex flex-col items-center justify-center ">
 
@@ -74,48 +73,49 @@ const Ludotheque = ({ allGamesFromAPI }) => {
                     <div className="py-10">
                         <div className="search rounded p-5">
                             <form action="" method="get" className="flex flex-col">
-                                <div className="form-group flex flex-nowrap items-center p-6">
-                                    <label htmlFor="searchByName">Rechercher</label>
+                                <div className="form-group flex flex-col p-3">
+                                    <label htmlFor="searchByName" className="font-bold">Rechercher :</label>
                                     <input
+                                        placeholder="Avez-vous un jeu en tête ?"
                                         type="text"
                                         id="searchByName"
-                                        className="search-ByName ml-6 p-2 rounded"
+                                        className="search-ByName p-2 w-full rounded"
                                         value={searchInputText}
                                         onChange={searchByName.bind(this)}
                                     />
                                 </div>
-                                <div className="flex flex-wrap p-3">
-                                    <div className="form-group flex flex-wrap m-3 items-center">
-                                        <label htmlFor="numberPlayer">Nombre de Joueurs</label>
+                                <div className="flex flex-col lg:flex-row p-3">
+                                    <div className="form-group flex flex-col p-1">
+                                        <label htmlFor="numberPlayer" className="font-bold">Nombre de Joueurs :</label>
                                         <input
                                             type="number"
                                             id="numberPlayer"
                                             value={searchInputPlayer}
                                             onChange={searchByNbPlayer.bind(this)}
-                                            className="ml-6 p-2 w-12 rounded" />
+                                            className="p-2 rounded" />
                                     </div>
-                                    <div className="form-group flex flex-wrap m-3 items-center">
-                                        <label htmlFor="timePlayable">Temps</label>
+                                    <div className="form-group flex flex-col p-1">
+                                        <label htmlFor="timePlayable" className="font-bold">Temps :</label>
                                         <select
                                             name=""
                                             id="timePlayable"
                                             value={searchInputTime}
                                             onChange={searchByTime.bind(this)}
-                                            className="p-2 ml-6 rounded">
+                                            className="p-2 rounded">
                                             <option value="(-15)">- 15min</option>
                                             <option value="(15 - 30)">15 - 30min</option>
                                             <option value="(30 - 60)">30 - 60min</option>
                                             <option value="(+60)">+ 60min</option>
                                         </select>
                                     </div>
-                                    <div className="form-group flex flex-wrap m-3 items-center">
-                                        <label htmlFor="difficulty">Difficulté</label>
+                                    <div className="form-group flex flex-col p-1">
+                                        <label htmlFor="difficulty" className="font-bold">Difficulté :</label>
                                         <select
                                             name=""
                                             id="difficulty"
                                             value={searchInputDifficulty}
                                             onChange={searchByDifficulty.bind(this)}
-                                            className="p-2 ml-6 rounded">
+                                            className="p-2 rounded">
                                             <option value="0">----</option>
                                             <option value="1">Facile</option>
                                             <option value="2">Intermédiaire</option>
@@ -124,57 +124,53 @@ const Ludotheque = ({ allGamesFromAPI }) => {
                                         </select>
                                     </div>
                                 </div>
-                                <div className="form-group flex flex-col p-4 col-span-1 md:col-span-2 py-6">
-                                    <label htmlFor="categories" className="py-4">Catégories</label>
-                                    <div className="w-full grid md:grid-cols-3 grid-flow-row gap-4 text-center" >
-                                        <div className="form-group-checkbox">
+                                <div className="form-group flex flex-col px-4 col-span-1 md:col-span-2 bg-red-500 rounded">
+                                    <label htmlFor="categories" className="pt-4 font-bold">Catégories :</label>
+                                    <div className="w-full grid md:grid-cols-3 grid-flow-row gap-4 p-2" >
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Adult">Adulte</label>
-                                            <input 
-                                            type="checkbox" 
-                                            id="Adult" 
-                                            name="" 
-                                            />
+                                            <input type="checkbox" id="Adult" name="" />
                                         </div>
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Ambiance">Ambiance</label>
                                             <input type="checkbox" id="Ambiance" name="" />
                                         </div>
 
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Bluff">Bluff</label>
                                             <input type="checkbox" id="Bluff" name="" />
                                         </div>
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Cards">Cartes</label>
                                             <input type="checkbox" id="Cards" name="" />
                                         </div>
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Coop">Coopération</label>
                                             <input type="checkbox" id="Coop" name="" />
                                         </div>
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Draw">Dessin</label>
                                             <input type="checkbox" id="Draw" name="" />
                                         </div>
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Dexterity">Dextérité</label>
                                             <input type="checkbox" id="Dexterity" name="" />
                                         </div>
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Child">Enfants</label>
                                             <input type="checkbox" id="Child" name="" />
                                         </div>
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Management">Gestion</label>
                                             <input type="checkbox" id="Management" name="" />
                                         </div>
-                                        <div className="form-group-checkbox">
+                                        <div className="form-group-checkbox flex items-center justify-between">
                                             <label htmlFor="Strategy">Stratégie</label>
                                             <input type="checkbox" id="Strategy" name="" />
                                         </div>
                                     </div>
                                 </div>
-                                <button className="submit rounded py-3 px-4 font-bold" type="submit">Rechercher</button>
+                                {/* <button className="submit rounded py-3 px-4 font-bold" type="submit">Rechercher</button> */}
                             </form>
                         </div>
                     </div>
@@ -183,7 +179,7 @@ const Ludotheque = ({ allGamesFromAPI }) => {
 
                         <h2>Résultats</h2>
                         <hr className="w-80" />
-                        <div className="grid grid-cols-4 gap-4">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-5">
                             {filteredGames.map(game => {
 
                                 //Shorten preamble
@@ -201,7 +197,7 @@ const Ludotheque = ({ allGamesFromAPI }) => {
                                 return (
 
                                     <Link href={'/ludotheque/' + game._id} key={game._id}>
-                                        <div className="gameBlock flex flex-col rounded-2xl p-5">
+                                        <div className="gameBlock flex flex-col rounded-2xl m-3 p-5 cursor-pointer transform hover:scale-110">
                                             <div className="fakeBlock self-center"> </div>
                                             <h4 className="font-semibold text-center">{game.title}</h4>
                                             <h4 className="desc">{desc}</h4>
