9af7d41f91e1d12899a219ef0b899841618abdda
Swany <swany59200@icloud.com>
2021-04-20 16:56:52 +0100

    Add of contact page

--- components/Reviews.js
+++ components/Reviews.js
@@ -8,33 +8,7 @@ export default function Reviews({ data }) {
         dots: true,
         speed: 600,
         fade: 500,
-        // slidesToScroll: 4,
-        // responsive: [
-        //     {
-        //       breakpoint: 1024,
-        //       settings: {
-        //         slidesToShow: 3,
-        //         slidesToScroll: 3,
-        //         infinite: true,
-        //         dots: true
-        //       }
-        //     },
-        //     {
-        //       breakpoint: 600,
-        //       settings: {
-        //         slidesToShow: 2,
-        //         slidesToScroll: 2,
-        //         initialSlide: 2
-        //       }
-        //     },
-        //     {
-        //       breakpoint: 480,
-        //       settings: {
-        //         slidesToShow: 1,
-        //         slidesToScroll: 1
-        //       }
-        //     }
-        //   ]
+        autoplay: true
     };
 
     const reviews = data.map((review) => {
@@ -50,8 +24,8 @@ export default function Reviews({ data }) {
         return (
             <div className="review text-white p-3">
                 {/* <img className="avatar" src="/assets/img/notes/avatar.svg" alt="" /> */}
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
                 <h3>{review.userName}</h3>
                 <div className="flex flex-row justify-center">
--- pages/404.js
+++ pages/404.js
@@ -2,17 +2,19 @@ import Head from "next/head";
 
 export default function NotFound() {
     return(
-        <div className="bg-notfound h-screen">
+        <div className="bg-notfound ">
         <Head >
             <title>La Luck - Page introuvable</title>
             <link rel="icon" href="/icon.ico" />
         </Head>
-            <div className="container flex flex-col items-center justify-center">
+        <div className="h-screen">
+            <div className="container flex flex-col items-center">
                 <h1>404</h1>
                 <img src="/assets/img/deco/dribbble_1.gif" alt=""/>
                 <h2>La page que vous cherchez semble introuvable.</h2>
                 <a href="/" className="link-notfound border-solid border-4 border-grey-500 p-4 rounded mt-2">Retour à l'accueil</a>
             </div>
+        </div>
         </div>
     )
 }
